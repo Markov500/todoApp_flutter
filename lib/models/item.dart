2d5fdf85fa219368d60be3c8b5625805284cdab0
Markov500 <markonolitse@gmail.com>
2023-04-10 20:36:59 +0000

    implementation de la fonction de d'ajout d'éléments

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -5,9 +5,5 @@ class Item {
 
   Item({required this.id, required this.texte, this.isDone = false});
 
-  static List<Item> items = [
-    Item(id: 1, texte: "Ananas"),
-    Item(id: 2, texte: "Banane", isDone: true),
-    Item(id: 3, texte: "Orange")
-  ];
+  static List<Item> items = [];
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:demo/constants/colors.dart';
 import 'package:demo/models/item.dart';
-import 'package:demo/widgets/add_item.dart';
 import 'package:demo/widgets/element_format.dart';
 import 'package:demo/widgets/my_app_bar.dart';
 import 'package:demo/widgets/search_field.dart';
@@ -15,6 +14,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final List<Item> myListItem = Item.items;
+  final inputController = TextEditingController();
 
   void handleItemChange(Item item) {
     setState(() {
@@ -26,6 +26,13 @@ class _HomeState extends State<Home> {
     var i = myListItem.where((element) => element.id == item.id).first;
     setState(() {
       myListItem.remove(i);
+    });
+  }
+
+  void handleItemAdd(String texteItem) {
+    int id = myListItem.last.id + 1;
+    setState(() {
+      myListItem.add(Item(id: id, texte: texteItem));
     });
   }
 
@@ -68,7 +75,58 @@ class _HomeState extends State<Home> {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: addItem(),
+
+            //Elément gérant l'ajout
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      bottom: 20,
+                      right: 10,
+                      left: 10,
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    decoration: BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: primaryColor,
+                            offset: Offset(0, 0),
+                            blurRadius: 10,
+                            spreadRadius: 0,
+                          ),
+                        ]),
+                    child: TextField(
+                      controller: inputController,
+                      decoration: const InputDecoration(
+                          hintText: "Elément à ajouter",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: primaryColor,
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 10,
+                    left: 10,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.add,
+                      color: secondaryColor,
+                    ),
+                    onPressed: () {
+                      handleItemAdd(inputController.text);
+                      inputController.clear();
+                    },
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
